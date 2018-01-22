b44e655ef1b5c03f2fdd926ef6926e3737c338f2
liningning <673216770@qq.com>
2018-01-22 10:16:49 +0800

    Mlp

--- src/test/java/com/rongzi/ms/step_definitions/mobile/mlp/MlpHongbaoSteps.java
+++ src/test/java/com/rongzi/ms/step_definitions/mobile/mlp/MlpHongbaoSteps.java
@@ -4,6 +4,8 @@ import com.rongzi.ms.pageobjects.mobile.mlp.MlpHongbaoLoginPage;
 import com.rongzi.ms.pageobjects.web.CenterIntelligentLoanPage;
 import com.rongzi.ms.step_definitions.StepDefs;
 import cucumber.api.PendingException;
+import cucumber.api.java.zh_cn.假如;
+import cucumber.api.java.zh_cn.当;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -13,12 +15,12 @@ import static com.rongzi.ms.pageobjects.BaseClass.driver;
 public class MlpHongbaoSteps extends StepDefs{
     private WebDriverWait wait = new WebDriverWait(driver, 10);
 
-    @cucumber.api.java.zh_cn.假如("^我在红包着陆页首页\"([^\"]*)\"$")
+    @假如("^我在红包着陆页首页\"([^\"]*)\"$")
     public void 我在红包着陆页首页(String HongbaoIndex) throws Throwable {
         driver.get(HongbaoIndex);
     }
 
-    @cucumber.api.java.zh_cn.当("^输入红包着陆页信息$")
+    @当("^输入红包着陆页信息$")
     public void 输入红包着陆页信息() throws Throwable {
         PageFactory.initElements(driver, MlpHongbaoLoginPage.CityPage.class);
         MlpHongbaoLoginPage.CityPage.city.click();
